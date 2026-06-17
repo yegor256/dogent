27a5882d4b796aac489996f00f11e5466f2a5462
Rultor.com <gpg@rultor.com>
2026-06-17 05:38:30 +0000

    set version to 0.9.1

--- src/version.js
+++ src/version.js
@@ -9,8 +9,8 @@
  * Version.
  *
  * The current release of dogent, replaced on every release by rultor.
- * The default `0.0.0` marks an unreleased build straight from source.
+ * The default `0.9.1` marks an unreleased build straight from source.
  */
-const version = '0.0.0';
+const version = '0.9.1';
 
 module.exports = version;
